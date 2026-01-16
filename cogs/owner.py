123a14848506bfb6c38d32465feedd9543d37eae
Rahmatur Ramadhan <42727186+rahmat412@users.noreply.github.com>
2026-01-16 14:35:48 +0700

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -38,30 +38,29 @@ class OwnerCog(commands.Cog, name="Owner-Only Commands"):
         [handler, handler_args] = self.get_command_handler_and_args(ctx, args)
         await handler.add_score(ctx, *handler_args)
 
-
-def get_command_handler_and_args(
-    self, ctx: commands.Context, args: tuple[str]
-) -> tuple[BaseCommandHandler, tuple[str]]:
-    match self.utils.get_game_from_channel(ctx.message):
-        case NYTGame.CONNECTIONS:
-            return self.connections, args
-        case NYTGame.STRANDS:
-            return self.strands, args
-        case NYTGame.WORDLE:
-            return self.wordle, args
-        case NYTGame.PIPS:
-            return self.pips, args
-        case NYTGame.UNKNOWN:
-            match self.utils.get_game_from_command(*args):
-                case NYTGame.CONNECTIONS:
-                    return self.connections, args[1:]
-                case NYTGame.STRANDS:
-                    return self.strands, args[1:]
-                case NYTGame.WORDLE:
-                    return self.wordle, args[1:]
-                case NYTGame.PIPS:
-                    return self.pips, args[1:]
-    return None, ()
+    def get_command_handler_and_args(
+        self, ctx: commands.Context, args: tuple[str]
+    ) -> tuple[BaseCommandHandler, tuple[str]]:
+        match self.utils.get_game_from_channel(ctx.message):
+            case NYTGame.CONNECTIONS:
+                return self.connections, args
+            case NYTGame.STRANDS:
+                return self.strands, args
+            case NYTGame.WORDLE:
+                return self.wordle, args
+            case NYTGame.PIPS:
+                return self.pips, args
+            case NYTGame.UNKNOWN:
+                match self.utils.get_game_from_command(*args):
+                    case NYTGame.CONNECTIONS:
+                        return self.connections, args[1:]
+                    case NYTGame.STRANDS:
+                        return self.strands, args[1:]
+                    case NYTGame.WORDLE:
+                        return self.wordle, args[1:]
+                    case NYTGame.PIPS:
+                        return self.pips, args[1:]
+        return None, ()
 
 
 async def setup(bot: commands.Bot):
